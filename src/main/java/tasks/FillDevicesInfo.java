8c7dc2a6d2a3314131ddca71203facf29293b900
JuanD <jgomezgo1849@universidadean.edu.co>
2021-08-05 13:40:12 -0500

    add a new commit

--- src/main/java/tasks/FillDevicesInfo.java
+++ src/main/java/tasks/FillDevicesInfo.java
@@ -5,7 +5,11 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import org.openqa.selenium.By;
 
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.containsText;
+
+import static net.serenitybdd.screenplay.questions.WebElementQuestion.the;
 import static userInterfaces.DevicesPage.*;
 
 public class FillDevicesInfo implements Task {
@@ -34,14 +38,23 @@ public class FillDevicesInfo implements Task {
                 Click.on(SLC_VERSION),
                 Click.on(By.xpath("//div[contains(text(),'" + version + "')]")),
                 Click.on(SLC_LANGUAGE),
-                // No puedo seleccionar un puto lenguaje
-                //Click.on(By.xpath("//div[contains(text(),'" + language + "')]")),
+                Click.on(By.xpath("//div[contains(text(),'"+ language +"')]")),
                 Click.on(SLC_MOBILE),
                 Click.on(By.xpath("//div[contains(text(),'" + mobile + "')]")),
                 Click.on(SLC_MODEL),
                 Click.on(By.xpath("//div[contains(text(),'" + model + "')]")),
                 Click.on(SLC_OS),
-                Click.on(By.xpath("//div[contains(text(),'" + os + "')]")),
+                Click.on(By.xpath("//div[contains(text(),'" + os + "')]"))
+        );
+        actor.should(
+                seeThat(the(By.xpath("//span[contains(text(),'"+ computer +"')]")), containsText(computer)),
+                seeThat(the(By.xpath("//span[contains(text(),'" + version + "')]")), containsText(version)),
+                seeThat(the(By.xpath("//span[contains(text(),'" + language + "')]")), containsText(language)),
+                seeThat(the(By.xpath("//span[contains(text(),'" + mobile + "')]")), containsText(mobile)),
+                seeThat(the(By.xpath("//span[contains(text(),'" + model + "')]")), containsText(model)),
+                seeThat(the(By.xpath("//span[contains(text(),'" + os + "')]")), containsText(os))
+        );
+        actor.attemptsTo(
                 Click.on(BTN_LAST_STEP)
         );
     }
