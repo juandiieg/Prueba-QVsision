e1919de17347ad41c5fd22a93bfc0f5c618abd46
JuanD <jgomezgo1849@universidadean.edu.co>
2021-08-04 22:26:13 -0500

    add a new commit

--- src/test/java/userInterface/RegisterObjects.java
+++ src/test/java/userInterface/RegisterObjects.java
@@ -19,7 +19,7 @@ public class RegisterObjects extends PageObject {
 	public static final Target INP_CITY = Target.the("City Input").located(By.id("city"));
 	public static final Target INP_ZIP = Target.the("Zip or Postal Code Input").located(By.id("zip"));
 	public static final Target SLC_COUNTRY = Target.the("Country Selection").located(By.xpath(
-			"//span[contains(text(),'Select a country')]"));
+			"//body/ui-view[1]/main[1]/section[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[1]/div[3]/div[1]/div[4]/div[2]/div[1]/div[1]/div[1]/span[1]"));
 	public static final Target BTN_NEXT2 = Target.the("Next Devices boton")
 			.located(By.xpath("//span[contains(text(),'Next: Devices')]"));
 }
